dcfe5af8d53c38cea8efff53f511822cf202b788
이태근 <@DESKTOP-HSNQGCF>
2017-04-26 17:15:55 +0900

    test

---
+++ JavaPractice/src/org/dimigo/inheritance/Chinese.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+/**/
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_Korean
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 26.
+ * </pre>
+ * asd
+ * @author 		: 이태근
+ * @version		: 1.0
+ */
+public class Chinese extends Person {
+	public Chinese(String name){
+		super(name);
+	}
+	public void sayHello(){
+		System.out.println("니하오");
+	}
+	public void sayBye(){
+		System.out.println("쨔이찌엔");
+	}
+	public String toString(){
+		return "저는 중국사람 "+ getName() +"입니다.";
+	}
+	
+	
+}
