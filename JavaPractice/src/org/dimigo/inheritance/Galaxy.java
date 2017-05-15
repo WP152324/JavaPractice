956dd6779f8b9cbee5ed90d8a24b596ad3f7fabb
WP152324 <you711789@naver.com>
2017-05-15 10:48:17 +0900

    Add files via upload

---
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_Galaxy
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 12.
+ * </pre>
+ *
+ * @author 		: 이태근
+ * @version		: 1.0
+ */
+public class Galaxy extends SmartPhone{
+	public Galaxy(){
+		
+	}
+	public Galaxy(String model, String company, int price){
+		super(model, company, price);
+	}
+	public void pay(){
+		System.out.println("삼성페이로 결제합니다.");
+	}
+	public void useWirelessCharging(){
+		System.out.println("무선충전기능을 사용합니다.");
+	}
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_SmartPhoneTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 12.
+ * </pre>
+ *
+ * @author 		: 이태근
+ * @version		: 1.0
+ */
+public class SmartPhoneTest {
+	public static void main(String[]args){
+		SmartPhone[] sps={
+			new IPhone("IPhone","애플",900000),
+			new Galaxy("갤럭시S8","삼성",800000),
+		};
+		for(SmartPhone phone: sps){
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction(phone);
+			phone.turnOff();
+			System.out.println();
+		}
+	}
+}
